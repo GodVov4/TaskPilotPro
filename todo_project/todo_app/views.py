faa72f20dea2e6661ee4f50e9b37f427db3cf724
GodVov4 <volume245@gmail.com>
2023-12-06 18:49:38 +0200

    init

---
+++ todo_project/todo_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+
+# Create your views here.
+class TodoListView(ListView):
+    pass
