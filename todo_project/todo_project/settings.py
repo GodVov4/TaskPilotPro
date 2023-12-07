57f2cdec4524c309564c7c9b035999f399b43569
GodVov4 <volume245@gmail.com>
2023-12-07 22:09:23 +0200

    add humanize

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'todo_app.apps.TodoAppConfig'
 ]
 
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'uk-ua'
 
 TIME_ZONE = 'UTC'
 
